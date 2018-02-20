98cc6c2ab19b12b77fcfe763ef4a37d5bfb7a006
EmilyGaoCoding <emily.s.gao@gmail.com>
2018-02-20 09:00:55 -0700

    move state to App.jsx

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,36 +2,20 @@ import React, {Component} from 'react';
 import Message from './Message.jsx';
 
 class MessageList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { messages: [{
-      id: 1,
-      username: 'Tom',
-      content: "I won't be impressed with technology until I can download food."
-    }, {
-      id: 2,
-      username: 'Jessica',
-      content: 'Wrong, telecommuting is the ultimate goal.'
-    }]};
-  }
-
-  setState() {
-
-  }
 
   render() {
-    const messages = this.state.messages.map(message => {
-      return (<Message id={message.id} username={message.username} content={message.content} />);
+    const messages = this.props.messages.map(message => {
+      return (<Message key={message.id} message={message} />);
     });
 
     return (
-      <main className="messages">
-        {messages}
-        <div className="message system">
-          Anonymous1 changed their name to nomnom.
-        </div>
-      </main>
+      <div>
+        <main className="messages">
+          {messages}
+        </main>
+      </div>
     );
   }
+
 }
 export default MessageList;
