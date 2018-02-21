0107dea16e6a2f7f292491d3cbf35906a88e95c7
EmilyGaoCoding <emily.s.gao@gmail.com>
2018-02-21 16:33:30 -0700

    display images if msg is an image url

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,16 +3,29 @@ import React, {Component} from 'react';
 class Message extends Component {
   render() {
     if (this.props.message.type === 'incomingMessage') {
-      return (
-        <div className="message">
-          <span className="message-username">{this.props.message.username}</span>
-          <span className="message-content">{this.props.message.content}</span>
-        </div>
-      );
+      console.log('incomingMessage reached Message.jsx');
+
+      const imgUrl = RegExp('^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png)$');
+      if (imgUrl.test(this.props.message.content)) {
+        return (
+          <div className="d-flex flex-row">
+            <div className="p-2" className={this.props.message.userColor}>{this.props.message.username}</div>
+            <div className="p-6 text-dark"><img src={this.props.message.content} alt="tree"/></div>
+          </div>
+        );
+      } else {
+        return (
+          <div className="d-flex flex-row">
+            <div className="p-2" className={this.props.message.userColor}>{this.props.message.username}</div>
+            <div className="p-6 text-dark">{this.props.message.content}</div>
+          </div>
+        );
+      }
     } else if (this.props.message.type === 'incomingNotification') {
+      console.log('incomingNotification reached Message.jsx');
       return (
-        <div className="message system">
-          {this.props.message.content}
+        <div className="d-flex flex-row">
+          <div className="p-6">{this.props.message.content}</div>
         </div>
       );
     }
