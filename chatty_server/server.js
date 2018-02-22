c6857445fe58b527a048134618c0b261cfa79c03
EmilyGaoCoding <emily.s.gao@gmail.com>
2018-02-21 17:38:40 -0700

    refactor: create broadcast function to send msgs to all open connections

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -2,13 +2,10 @@ const express = require('express');
 const WebSocket = require('ws');
 const SocketServer = require('ws').Server;
 const uuidv4 = require('uuid/v4');
-
 const PORT = 3001;
-
 const server = express()
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on PORT ${ PORT }`));
-
 const wss = new SocketServer({ server });
 
 const colors = ['text-success', 'text-info', 'text-warning', 'text-danger'];
@@ -41,6 +38,17 @@ function assignUserColor(ws) {
   ws.send(JSON.stringify(userColorMsg));
 }
 
+// add new message to messages array and send to every open connection
+function broadcast(message) {
+  message.id = uuidv4();
+  messages.push(message);
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  });
+}
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
   logNumUsersOnline();
@@ -48,36 +56,44 @@ wss.on('connection', (ws) => {
   messages.splice(-10).forEach(message => {
     ws.send(JSON.stringify(message));
   })
-
+  const username = 'Anonymous';
+  
+  // notify all that a new user has joined
+  const userJoin = {
+    type: 'incomingNotification',
+    content: 'A new user has joined the chat',
+  }
+  broadcast(userJoin);
+  
   // broadcast messages to all users
   ws.on('message', (message) => {
     const outgoingMsg = JSON.parse(message);
-    outgoingMsg.id = uuidv4();
-
+    
     switch(outgoingMsg.type) {
       case 'postNotification': 
-        outgoingMsg.type = 'incomingNotification';
-        break;
+      username = outgoingMsg.newName;
+      outgoingMsg.type = 'incomingNotification';
+      break;
       case 'postMessage': 
-        outgoingMsg.type = 'incomingMessage';
-        break;
+      outgoingMsg.type = 'incomingMessage';
+      break;
       default: break;
     }
-    console.log(outgoingMsg);
-    messages.push(outgoingMsg);
-
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(outgoingMsg));
-      }
-    });
+    
+    broadcast(outgoingMsg);
   });
-
+  
   ws.on('close', () => {
     console.log('Client disconnected');
     logNumUsersOnline();
+    
+    const userLeave = {
+      type: 'incomingNotification',
+      content: `${username} has left the chat`,
+    }
+    broadcast(userLeave);
   });
-
+  
   ws.on('error', () => {
     console.log('There is an error');
   });
