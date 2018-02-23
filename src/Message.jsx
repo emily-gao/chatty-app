bf41d315d9dfa9e4cb5166a7c7dbe4e85d4fcce7
EmilyGaoCoding <emily.s.gao@gmail.com>
2018-02-22 18:03:00 -0700

    align components

--- src/Message.jsx
+++ src/Message.jsx
@@ -8,24 +8,32 @@ class Message extends Component {
       const imgUrl = RegExp('^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png)$');
       if (imgUrl.test(this.props.message.content)) {
         return (
-          <div className="d-flex flex-row">
-            <div className="p-2" className={this.props.message.userColor}>{this.props.message.username}</div>
-            <div className="p-6 text-dark"><img src={this.props.message.content} alt="tree"/></div>
+          <div className="message row">
+            <div className="col-4">
+              <span className={this.props.message.userColor}>{this.props.message.username}</span>
+            </div>
+            <div className="col-8">
+              <img src={this.props.message.content} alt="tree"/>
+            </div>      
           </div>
         );
       } else {
         return (
-          <div className="d-flex flex-row">
-            <div className="p-2" className={this.props.message.userColor}>{this.props.message.username}</div>
-            <div className="p-6 text-dark">{this.props.message.content}</div>
+          <div className="message row">
+            <div className="col-4">
+              <span className={this.props.message.userColor}>{this.props.message.username}</span>
+            </div>
+            <div className="col-8">
+              <span className="text-dark">{this.props.message.content}</span>
+            </div>      
           </div>
         );
       }
     } else if (this.props.message.type === 'incomingNotification') {
       console.log('incomingNotification reached Message.jsx');
       return (
-        <div className="font-italic d-flex flex-row">
-          <div className="p-6">{this.props.message.content}</div>
+        <div className="font-italic">
+          <div className="col-12 text-muted">{this.props.message.content}</div>
         </div>
       );
     }
